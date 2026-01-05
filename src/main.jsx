0174f801a7416a314f8c2ae7cc6d648a92bfec66
(Raooph) <(raufquliyev829@gmail.com)>
2026-01-05 20:58:16 +0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-import './index.css'
-import App from './App.jsx'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-)
+  </HashRouter>
+);
